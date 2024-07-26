1c4d6014f9a892431c922dd7153e9bc1938b7912
Lucsallk <lucsallk@hotmail.com>
2024-07-25 23:08:48 -0300

    feat: adding the timeline

---
+++ src/data/timeline.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    year: "Hoje",
+    title: "Titulo 1",
+    duration: "1 anos",
+    details:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi sequi veritatis impedit magnam debitis fugit quis soluta minima explicabo aliquam molestiae minus doloribus illum nihil dolor earum vitae, laboriosam laudantium!",
+  },
+  {
+    year: "Hoje",
+    title: "Titulo 1",
+    duration: "1 anos",
+    details:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi sequi veritatis impedit magnam debitis fugit quis soluta minima explicabo aliquam molestiae minus doloribus illum nihil dolor earum vitae, laboriosam laudantium!",
+  },
+  {
+    year: "Ano passado",
+    title: "Titulo 2",
+    duration: "3 anos",
+    details:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi sequi veritatis impedit magnam debitis fugit quis soluta minima explicabo aliquam molestiae minus doloribus illum nihil dolor earum vitae, laboriosam laudantium!",
+  },
+  {
+    year: "Ano retrasado",
+    title: "Titulo 3",
+    duration: "3 anos",
+    details:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi sequi veritatis impedit magnam debitis fugit quis soluta minima explicabo aliquam molestiae minus doloribus illum nihil dolor earum vitae, laboriosam laudantium!",
+  },
+];
